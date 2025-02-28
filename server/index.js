814b39d081cb2f21840f59b4c3239685f722622c
arafatDU <bsse1435@iit.du.ac.bd>
2025-02-28 16:04:02 +0600

    Socket initiated on server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
-console.log('Server is running...');
+const { Server } = require('socket.io');
+
+const io = new Server(8000, {
+  cors: true,
+});
+
+
+io.on("connection", (socket) => {
+  console.log(`Socket connected: ${socket.id}`);
+  socket.on("message", (data) => {
+    console.log(data);
+    io.emit("message", data);
+  }); 
+})
