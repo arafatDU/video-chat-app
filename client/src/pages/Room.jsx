d6ee8146e224a366ab610ca5b6ecb76863977eb1
arafatDU <bsse1435@iit.du.ac.bd>
2025-03-01 19:31:41 +0600

    Socket event "user:joined" for joining sockets in one room

--- client/src/pages/Room.jsx
+++ client/src/pages/Room.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useSocket } from '../context/SocketProvider';
 
 const Room = () => {
-  const { roomId } = useParams();
+  const socket = useSocket();
+
+  const handleUserJoined = ({email, id}) => {
+    console.log(email, id);
+  }
+  
+  useEffect(() => {
+    socket.on("user:joined", handleUserJoined);
+    return () => {
+      socket.off("user:joined");
+    }
+  }, [socket, handleUserJoined]);
 
   return (
-    <div>
-      <h1>Room ID: {roomId}</h1>
+    <div className='flex items-center justify-center min-h-screen bg-gray-100'>
+      <h1>Room</h1>
     </div>
   );
 };
