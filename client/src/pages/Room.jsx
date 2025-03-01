53071966aa441bc29ab87fbcb707f351e2c6d185
arafatDU <bsse1435@iit.du.ac.bd>
2025-03-02 05:53:58 +0600

    My stream setup in Room

--- client/src/pages/Room.jsx
+++ client/src/pages/Room.jsx
@@ -1,12 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useSocket } from '../context/SocketProvider';
+import ReactPlayer from 'react-player';
 
 const Room = () => {
   const socket = useSocket();
+  const [remoteSocketId, setRemoteSocketId] = useState(null);
+  const [myStream, setMyStream] = useState(null);
 
   const handleUserJoined = ({email, id}) => {
     console.log(email, id);
+    setRemoteSocketId(id);
   }
+
+  const handleCallUser = useCallback(async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+    setMyStream(stream);
+  }, []);
   
   useEffect(() => {
     socket.on("user:joined", handleUserJoined);
@@ -16,8 +25,20 @@ const Room = () => {
   }, [socket, handleUserJoined]);
 
   return (
-    <div className='flex items-center justify-center min-h-screen bg-gray-100'>
-      <h1>Room</h1>
+    <div className='items-center justify-center min-h-screen bg-gray-100'>
+      <h1 className='text-5xl font-bold text-center p-4'>Room Page</h1>
+      <br />
+      <h4 className='text-2xl font-bold text-center'> {remoteSocketId ? 'Connected' : 'No one in Room'} </h4>
+
+      <div className='flex justify-center items-center'>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold mt-5 py-2 px-4 rounded' onClick={handleCallUser}>CALL
+        </button>
+      </div>
+      
+      <div className='mt-5 p-5'>
+        <h3 className='text-2xl m-3 py-3'>My Stream</h3>
+        {myStream && <ReactPlayer url={myStream}  playing={true} height="200px" width="300px" muted/>}
+      </div>
     </div>
   );
 };
